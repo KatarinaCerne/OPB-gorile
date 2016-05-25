f8160189c7858a149e5317009442175d8b0bd16a
Katarina Černe <ktrn.crn@gmail.com>
2016-05-25 21:50:31 +0200

    neuspeli poskusi grafov

---
+++ Shiny/server.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(dplyr)
+library(RPostgreSQL)
+library(ggplot2)
+library(plotrix)
+
+source("auth.R")
+
+shinyServer(function(input, output) {
+  # Vzpostavimo povezavo
+  conn <- src_postgres(dbname = db, host = host,
+                       user = user, password = password)
+  # Pripravimo tabele
+  tbl.zlocin <- tbl(conn, "zlocin")
+  tbl.postopek <- tbl(conn, "postopek")
+  tbl.preiskava <- tbl(conn, "preiskava")
+  #tbl.lokacija <- tbl(conn, "lokacija") tole zakomentiramo, ni v bazi
+  zbl.lsoa <- tbl(conn, "lsoa")
+  
+  output$zlocini <- renderTable({
+    t <- tbl.zlocin %>% data.frame()
+    t
+  })
+  output$postopki <- renderTable(
+    {
+      t1 <- tbl.postopek %>% data.frame()
+      t1
+    }
+  )
+  output$postopkiPita <- renderPlot({
+  plotData <- tbl.zlocin %>% select(status) %>% data.frame() %>% table()
+  oznake <- paste(names(plotData), "\n", plotData, sep="")
+  
+  pie3D(plotData, labels = oznake, explode = 0.1, main = "Zlocini")
+  
+  #barp(plotData,width=0.4,names.arg=oznake,legend.lab=NULL,legend.pos=NULL,
+  #     col=NULL,border=par("fg"),main="zlocini",xlab="",ylab="",xlim=NULL,ylim=NULL,
+  #     x=NULL,staxx=FALSE,staxy=FALSE, height.at=NULL,height.lab=NULL,
+  #     cex.axis=par("cex.axis"),pch=NULL,cylindrical=FALSE,shadow=FALSE,
+  #     do.first=NULL,ylog=FALSE,srt=NA)
+  #  })
+
+  
+}
+)
+})
---
+++ Shiny/ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+shinyUI(navbarPage("Zlocini UK",
+                   tabPanel("Domov",
+                            mainPanel("Nahajate se v aplikaciji Zlocini UK, kjer so zbrani osnovni podatki o zlocinih v UK.",
+                                      tags$br(),
+                                      "V naslednjem zavihku je tabela zlocinov, vendar moras malcek pocakati, da se nalozi.")
+                   ),
+                   tabPanel("Tabelica zlocinov",
+                            sidebarLayout(
+                              sidebarPanel(plotOutput("postopkiPita")),
+                              mainPanel(tableOutput("zlocini"))
+                            )
+                      
+                   ),
+                   tabPanel("Postopki",
+                            sidebarLayout(
+                              sidebarPanel(""),
+                              mainPanel(tableOutput("postopki"))
+                            ))
+)
+)
